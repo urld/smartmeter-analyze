1b29855796488ad0ebbe5ef6d682d911231b93f7
David Url <david@x00.at>
2014-08-16 23:40:52 +0200

    used pandas for data analysis

--- smartmeter/analyze.py
+++ smartmeter/analyze.py
@@ -1,41 +1,50 @@
 #!/usr/bin/python
 
-import csv
+import pandas as pd
+# import numpy as np
 
 FILE = '../test/tageswerte-20140816-174538.csv'
-
-
-def _read_csv(filename, delimiter=';'):
-    with open(filename, 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=delimiter)
-        for row in reader:
-            yield row
+# SEP_LINE = "=================================="
+SEP_LINE = '='*33
 
 
 def read_consumption_csv(filename):
-    # TODO: error handling
-    consumption_csv = _read_csv(filename, delimiter=';')
-    # header = consumption_csv.next()
-    consumption_csv.next()  # skip header
-    consumptions = [{'date': day[0],
-                    'consumption': float(day[1].replace(',', '.'))}
-                    for day in consumption_csv]
+    consumptions = pd.read_csv(FILE,
+                               delimiter=';',
+                               header=0,  # ignore header
+                               usecols=[0, 1],  # third column is empty
+                               names=['date', 'consumption'],
+                               index_col='date',  # use date as index
+                               decimal=',',
+                               parse_dates=True,
+                               infer_datetime_format=True,
+                               dayfirst=True,  # csv dateformat: DD.MM.YYYY
+                               )
     return consumptions
 
 
-def print_summary(consumptions):
-    # TODO: use pandas dataframe for nicer output
-    values = map(lambda c: c['consumption'], consumptions)
-    count = len(consumptions)
-    total = sum(values)
-    average = total / count
-    print "Days:\t{}".format(count)
-    print "Sum:\t{} kWh".format(total)
-    print "Avg:\t{} kWh".format(round(average, 3))
-    print "Max:\t{} kWh".format(max(values))
-    print "Min:\t{} kWh".format(min(values))
+def print_summary(data):
+    aggregates = [[data.consumption.sum()],
+                  [data.consumption.mean()],
+                  ]
+    extrema = [[data.consumption.idxmin(), data.consumption.min()],
+               [data.consumption.idxmax(), data.consumption.max()],
+               ]
+    print "CONSUMPTION SUMMARIES"
+    print SEP_LINE
+    print "from {}".format(data.index.min().date())
+    print "to   {}".format(data.index.max().date())
+    print "({} days)".format(data.consumption.count())
+    print SEP_LINE
+    print pd.DataFrame(aggregates,
+                       columns=['consumption [kWh]'],
+                       index=['sum', 'average'])
+    print SEP_LINE
+    print pd.DataFrame(extrema,
+                       columns=['date', 'consumption [kWh]'],
+                       index=['min', 'max'])
 
 
 if __name__ == "__main__":
-    consumptions = read_consumption_csv(FILE)
-    print_summary(consumptions)
+    data = read_consumption_csv(FILE)
+    print_summary(data)
