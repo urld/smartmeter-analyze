f4b33b0818d25d95898f7cc175dd7064c16d45b7
David Url <david@x00.at>
2014-08-17 16:48:36 +0200

    added capability to search directories for csv files

--- smartmeter-util.py
+++ smartmeter-util.py
@@ -2,28 +2,35 @@
 """Smartmeter Util.
 
 Usage:
-    {basename} summary [options] <filename>...
-    {basename} weekly [options] <filename>...
-    {basename} import [options] <filename>...
+    {basename} summary [options] [-p <pattern>] <filename>...
+    {basename} weekly [options] [-p <pattern>] <filename>...
+    {basename} import [options] [-p <pattern>] <filename>...
     {basename} -h | --help
     {basename} --version
 
 Options:
-    -h --help   Show help.
-    --debug     Print debugging messages.
+    -h --help       Show help.
+    --debug         Print debugging messages.
+    -p <pattern>    Pattern to filter csv files if one or more directories are
+                    specified in the file list. [default: tageswerte*.csv]
 """
 
 from __future__ import print_function
+import sys
 
 from docopt import docopt
 from smartmeter import analyze
 
 if __name__ == "__main__":
-    arguments = docopt(__doc__.format(basename=__file__), version='0.1.0')
-    data = analyze.read_consumption_csv(arguments['<filename>'])
-    if arguments['--debug']:
-        print(arguments)
-    if arguments['weekly']:
+    args = docopt(__doc__.format(basename=__file__), version='0.1.0')
+    if args['--debug']:
+        print(args)
+    try:
+        data = analyze.read_consumption_csv(args['<filename>'], args['-p'])
+    except analyze.InputError as e:
+        print(e)
+        sys.exit(1)
+    if args['weekly']:
         analyze.print_stats_week(data)
     else:
         analyze.print_summary(data)
--- smartmeter/analyze.py
+++ smartmeter/analyze.py
@@ -1,3 +1,6 @@
+import os
+from fnmatch import fnmatch
+
 import pandas as pd
 import numpy as np
 
@@ -6,9 +9,43 @@ SEP_LINE = '='*33
 pd.set_option('display.precision', 4)
 
 
-def read_consumption_csv(filenames):
-    if isinstance(filenames, str):
-        filenames = [filenames]
+class InputError(Exception):
+
+    def __init__(self, msg, input=None):
+        self.msg = msg
+        self.input = input
+
+    def __str__(self):
+        if self.input:
+            return "{msg}: {input}".format(msg=self.msg, input=self.input)
+        else:
+            return str(self.msg)
+
+
+def _get_filelist_from_dir(dir, pattern):
+    filelist = []
+    for root, dirs, files in os.walk(dir, followlinks=True):
+        filelist += [os.path.join(root, f)
+                     for f in files if fnmatch(f, pattern)]
+    return filelist
+
+
+def _prepare_filelist(arg, pattern):
+    filenames = []
+    for path in arg:
+        if os.path.isdir(path):
+            filenames += _get_filelist_from_dir(path, pattern)
+        elif os.path.isfile(path):
+            filenames += [path]
+        else:
+            raise InputError('CSV files not found', input=path)
+    return filenames
+
+
+def read_consumption_csv(filenames, pattern):
+    filenames = _prepare_filelist(filenames, pattern)
+    if not filenames:
+        raise InputError('CSV files not found.')
     usages = []
     for filename in filenames:
         usages += [pd.read_csv(filename,
